3d444e3264cf046bc38b3b6c4509890cf11aa53b
Diana Gudu <diana.gudu@kit.edu>
2021-06-17 16:25:27 +0200

    add version option

--- mc_ssh/mccli.py
+++ mc_ssh/mccli.py
@@ -35,6 +35,7 @@ def common_options(func):
                      envvar=["OIDC_ISS", "OIDC_ISSUER"], show_envvar=True,
                      help="url of token issuer; configured account in oidc-agent for this issuer will be used")
     @click_logging.simple_verbosity_option(logger, "--log-level", default="ERROR", metavar="LEVEL")
+    @click.version_option()
     @wraps(func)
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
