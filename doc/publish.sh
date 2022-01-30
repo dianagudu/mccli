9788d98999ea42c4b3664ffa665b8132dae6c736
Diana Gudu <diana.gudu@kit.edu>
2022-01-30 23:09:39 +0100

    add tox directive to publish docs

---
+++ doc/publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf /tmp/gh-pages
+cp -r doc/build /tmp/gh-pages
+git checkout gh-pages
+rm -rf * && cp -r /tmp/gh-pages/* ./ && rm -rf /tmp/gh-pages & git add . && git commit -m "Updated gh-pages" && git push &&git checkout main
