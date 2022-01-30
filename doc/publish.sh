59323f7041403814ac4172b8b7c6b71dac2d7582
Diana Gudu <diana.gudu@kit.edu>
2022-01-30 23:32:33 +0100

    update script to publish docs

--- doc/publish.sh
+++ doc/publish.sh
@@ -2,5 +2,4 @@
 
 rm -rf /tmp/gh-pages
 cp -r doc/build /tmp/gh-pages
-git checkout gh-pages
-rm -rf * && cp -r /tmp/gh-pages/* ./ && rm -rf /tmp/gh-pages & git add . && git commit -m "Updated gh-pages" && git push &&git checkout main
+git checkout gh-pages && rm -rf * && cp -r /tmp/gh-pages/* ./ && rm -rf /tmp/gh-pages & git add . && git commit -m "Updated gh-pages" && git push &&git checkout main
