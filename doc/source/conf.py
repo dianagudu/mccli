cdd72d2e57aecab87e380c6b21f8e911a5f143db
Diana Gudu <diana.gudu@kit.edu>
2022-01-31 11:07:31 +0100

    add favicon to static path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -147,7 +147,10 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = [
+    '_static',
+    '_static/logos/mccli.ico'
+]
 
 #html_logo = '_static/logos/mccli-white.svg'
 
